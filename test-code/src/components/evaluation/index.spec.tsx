333af35888f078095ea1f7599da0f07cedd4924e
tigger <fronttigger@gmail.com>
2022-07-08 23:54:32 +0900

    msw testing code 적용 중

--- test-code/src/components/evaluation/index.spec.tsx
+++ test-code/src/components/evaluation/index.spec.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
 import Evaluation from './index'
+
+const review = {
+  rating: 5,
+  commentCount: 200,
+}
+
+const server = setupServer(
+  rest.get('/review', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(review))
+  })
+)
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 describe('Evaluation', () => {
   test('type을 넘기지 않으면 EvaluationGroup 컴포넌트가 렌더링된다.', () => {
@@ -12,7 +29,7 @@ describe('Evaluation', () => {
     expect(svgImages).toHaveLength(5)
   })
 
-  test('type을 review로 하면 후기 텍스트가 포함된 컴포넌트가 렌더링된다.', () => {
+  test('type을 review로 하면 후기 텍스트가 포함된 컴포넌트가 렌더링된다.', async () => {
     render(<Evaluation type='review' />)
 
     const evaluationReviewTextElement = screen.getByText('후기')
@@ -24,6 +41,11 @@ describe('Evaluation', () => {
 
   test('type을 review로 하고 후기 갯수를 200개 선언하면 후기 갯수 200개가 포함된 컴포넌트가 렌더링된다.', async () => {
     render(<Evaluation type='review' rating={5} commentCount={200} />)
+
+    const response = await fetch('/review')
+    const data = await response.json()
+
+    console.log('data', data)
 
     const evaluationReviewTextElement = screen.getByText('후기')
     const evaluationReviewCountElement = await screen.findByText('200')
