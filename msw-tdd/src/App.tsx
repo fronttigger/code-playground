b667dd1191428b70a6b663dbcb8b933d3e5ce85d
tigger <fronttigger@gmail.com>
2022-08-06 00:49:13 +0900

    component useQuery call

--- msw-tdd/src/App.tsx
+++ msw-tdd/src/App.tsx
@@ -1,7 +1,13 @@
+import { useQuery } from 'react-query'
 import List from './components/List'
+import { getTodos } from './remotes/todos'
 
 function App() {
-  return <List todos={[]} />
+  const { data: todos = [] } = useQuery(['http://localhost:8888/todos'], () =>
+    getTodos()
+  )
+
+  return <List todos={todos} />
 }
 
 export default App
