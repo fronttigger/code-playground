5c1be484e7f85cec95ae4f53e73364abc9d14af5
tigger <fronttigger@gmail.com>
2022-12-14 23:10:24 +0900

    Stack push 구현

--- data-structure/src/Stack/index.spec.ts
+++ data-structure/src/Stack/index.spec.ts
@@ -1,1 +1,15 @@
 import Stack from './index'
+
+describe('Stack', () => {
+  test('Stack 생성하면 빈 리스트가 생성된다.', () => {
+    const stack = new Stack()
+
+    expect(stack.list.size).toEqual(0)
+  })
+
+  //   describe('insertAt', () => {
+  //     test('비어있는 연결리스트에 1 이상의 인덱스에 데이터를 삽입시 에러가 발생한다.', () => {
+  //       const stack = new Stack()
+  //     })
+  //   })
+})
--- data-structure/src/Stack/index.ts
+++ data-structure/src/Stack/index.ts
@@ -1,3 +1,15 @@
-class Stack {}
+import LinkedList from '../LinkedList'
+
+class Stack<T> {
+  list: LinkedList<T>
+
+  constructor() {
+    this.list = new LinkedList()
+  }
+
+  push(data: T) {
+    this.list.insertAt(0, data)
+  }
+}
 
 export default Stack
